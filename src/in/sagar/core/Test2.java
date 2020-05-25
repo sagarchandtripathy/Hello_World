903ef17b270cd08d23c4db3524aa4c0dd09309aa
Sager <Sager@LAPTOP-S3OE47JT>
2020-05-25 17:09:15 +0530

    Added Test2 class. Closes #2

---
+++ src/in/sagar/core/Test2.java
@@ -0,0 +1,23 @@
+package in.sagar.core;
+
+public class Test2 {
+
+	private String name;
+	
+	
+
+	public Test2(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
