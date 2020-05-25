715127816ec2910c144d570a971074f938f8567a
Sager <Sager@LAPTOP-S3OE47JT>
2020-05-25 16:08:27 +0530

    add Test class. Second Commit

---
+++ src/in/sagar/core/Test.java
@@ -0,0 +1,17 @@
+package in.sagar.core;
+
+public class Test {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
